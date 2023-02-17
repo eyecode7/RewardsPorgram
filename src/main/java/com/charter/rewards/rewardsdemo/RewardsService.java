1b4ff3dc9d49ffb87eba690ccb587ff9e4546b49
eyecode7 <125624540+eyecode7@users.noreply.github.com>
2023-02-16 23:05:09 -0500

    Update RewardsService.java

--- src/main/java/com/charter/rewards/rewardsdemo/RewardsService.java
+++ src/main/java/com/charter/rewards/rewardsdemo/RewardsService.java
@@ -19,57 +19,56 @@ public class RewardsService {
 	int totalPoints;
 	
 	public CustomerBean createPurchase(CustomerBean customerBean) {
-		
+
 		customerBean.setRewardsPoints(calculateRewards(customerBean.getPurchaseAmount()));
-		
-		
+
 		return customerRepository.save(customerBean);
 	}
-	
+
 	public List<CustomerBean> createPurchases(List<CustomerBean> customerBean) {
 		List<CustomerBean> rewardsCustomer = customerBean;
-		
+
 		for (CustomerBean customer : rewardsCustomer) {
 			customer.setRewardsPoints(calculateRewards(customer.getPurchaseAmount()));
 		}
-		
-		
+
 		return customerRepository.saveAll(rewardsCustomer);
 	}
-	
+
 	public CustomerBean getCustomer(int id) {
 		return customerRepository.findById(id).orElse(null);
 	}
-	
+
 	public List<CustomerBean> getAllCustomer() {
-		calculateMonthlyandTotalRewards();
 		return customerRepository.findAll();
 	}
-	
+
 	public int calculateRewards(int purchaseAmount) {
-		//A customer receives 2 points for every dollar spent over $100 in each transaction, plus 1 point for every dollar spent between $50 and $100 in each transaction.
-		//(e.g. a $120 purchase = 2x$20 + 1x$50 = 90 points).
+		// A customer receives 2 points for every dollar spent over $100 in each
+		// transaction, plus 1 point for every dollar spent between $50 and $100 in each
+		// transaction.
+		// (e.g. a $120 purchase = 2x$20 + 1x$50 = 90 points).
 		int rewards = 0;
-		
-	if (purchaseAmount > 100){
-			
+
+		if (purchaseAmount > 100) {
+
 			points = purchaseAmount - 100;
-			twoPointRewards= points * 2;
+			twoPointRewards = points * 2;
 			rewards = twoPointRewards + 50;
-			System.out.println("purchaseAmount "+ purchaseAmount + " twoPointRewards " + twoPointRewards + " totalPoints " + rewards );
-			
-			
-		}if (purchaseAmount <= 100 && purchaseAmount > 50  ){
-				
-			onePointRewards= purchaseAmount - 50;
+			System.out.println("purchaseAmount " + purchaseAmount + " twoPointRewards " + twoPointRewards
+					+ " totalPoints " + rewards);
+
+		}
+		if (purchaseAmount <= 100 && purchaseAmount > 50) {
+
+			onePointRewards = purchaseAmount - 50;
 			rewards = onePointRewards;
-			System.out.println("onePointRewards " + rewards );
-			
-		}else if (purchaseAmount <= 50) {
+			System.out.println("onePointRewards " + rewards);
+
+		} else if (purchaseAmount <= 50) {
 			System.out.println("Sorry no rewards :(");
 		}
-		
-		
+
 		return rewards;
 	}
 	
@@ -85,7 +84,7 @@ public class RewardsService {
 			crb.setName(rewardsFromDB.getName());
 			crb.setTotalRewardsPoints(rewardsFromDB.getTotalRewardsPoints());
 			custRewardsList.add(crb);
-			System.out.println("Name " + rewardsFromDB.getName() + " total Rewards " + rewardsFromDB.getTotalRewardsPoints());
+//			System.out.println("Name " + rewardsFromDB.getName() + " total Rewards " + rewardsFromDB.getTotalRewardsPoints());
 		}
 
 		for (MonthlyTotal monthlyTotalFromDB : monthlyTotal) {
@@ -95,8 +94,8 @@ public class RewardsService {
 			mb.setName(monthlyTotalFromDB.getName());
 			mb.setMonth(monthlyTotalFromDB.getRewardMonth());
 			mb.setTotalPoints(monthlyTotalFromDB.getTotalPointsPerMonth());
-			System.out.println("Name " + monthlyTotalFromDB.getName() + " total Rewards Monthly " + monthlyTotalFromDB.getTotalPointsPerMonth()
-					+ " Reward Month " + monthlyTotalFromDB.getRewardMonth());
+//			System.out.println("Name " + monthlyTotalFromDB.getName() + " total Rewards Monthly " + monthlyTotalFromDB.getTotalPointsPerMonth()
+//					+ " Reward Month " + monthlyTotalFromDB.getRewardMonth());
 			monthlyRewardsList.add(mb);
 
 		}
